8b016f96a9a1a53391a6fc08356b19b14cf9ee0c
Carlos Selman <carlitosselman@gmail.com>
2021-11-29 17:10:18 -0600

    Estructuración y avances en la vista de MySQL

--- portafolio/src/app/app-routing.module.ts
+++ portafolio/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { MysqlDbComponent } from './componentes/mysql-db/mysql-db.component';
 
 const routes: Routes = [
+  { path: 'mysql', component: MysqlDbComponent},
   { path: 'inicio', component: InicioComponent},
   { path: '**', component: InicioComponent} 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{useHash:true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
